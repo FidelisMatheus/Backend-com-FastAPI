90b1e9886c721bdd1c8e0cba380fcdcb4270bb18
Matheus Fidelis <matheusfidelis@yahoo.com>
2024-05-15 08:39:39 -0300

    Adicionando o status code na aplicação

--- blx-backend/src/server.py
+++ blx-backend/src/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status
 from sqlalchemy.orm import Session
 from src.schemas.schemas import Produto
 from src.infra.sqlalchemy.config.database import criar_bd, get_db
@@ -9,13 +9,13 @@ criar_bd()
 appl = FastAPI()
 
 
-@appl.post("/produtos")
+@appl.post("/produtos", status_code=status.HTTP_201_CREATED)
 def criar_produto(produto: Produto, db: Session = Depends(get_db)):
     produto_criado = RepositorioProduto(db).criar(produto)
     return produto_criado
 
 
-@appl.get("/produtos")
+@appl.get("/produtos", status_code=status.HTTP_200_OK)
 def listar_produtos(db: Session = Depends(get_db)):
     produtos = RepositorioProduto(db).listar()
     return produtos
