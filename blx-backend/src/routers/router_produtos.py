caf2d4467826b21bc50f8c24d900f52f858d2dd1
Matheus Fidelis <matheusfidelis@yahoo.com>
2024-05-17 18:18:13 -0300

    Pesquisa por ID e tratamento de erros

--- blx-backend/src/routers/router_produtos.py
+++ blx-backend/src/routers/router_produtos.py
@@ -1,5 +1,5 @@
 from typing import Sequence
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, HTTPException
 from sqlalchemy.orm import Session
 from src.schemas.schemas import Produto, ProdutoSimples
 from src.infra.sqlalchemy.config.database import get_db
@@ -9,14 +9,29 @@ router = APIRouter()
 
 
 @router.get("/produtos", status_code=status.HTTP_200_OK)
-async def listar_produtos(db: Session = Depends(get_db)) -> Sequence[ProdutoSimples]:
-    produtos = RepositorioProduto(db).listar()
+async def listar_produtos(
+    session: Session = Depends(get_db),
+) -> Sequence[ProdutoSimples]:
+    produtos = RepositorioProduto(session).listar()
     return produtos
 
 
+@router.get("/produtos/{id}", response_model=list[Produto])
+def exibir_produto(id: int, session: Session = Depends(get_db)):
+    produto_localizado = RepositorioProduto(session).buscarPorId(id)
+
+    if not produto_localizado:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Não há um produto com id = {id}",
+        )
+
+    return produto_localizado
+
+
 @router.post("/produtos", status_code=status.HTTP_201_CREATED)
-async def criar_produto(produto: Produto, db: Session = Depends(get_db)):
-    produto_criado = RepositorioProduto(db).criar(produto)
+async def criar_produto(produto: Produto, session: Session = Depends(get_db)):
+    produto_criado = RepositorioProduto(session).criar(produto)
     return produto_criado
 
 
